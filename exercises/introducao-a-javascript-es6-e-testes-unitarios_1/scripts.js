67f94c74032622f45c622468e19d3ccfb3354eac
brutibbc <brutibbc@gmail.com>
2020-11-23 16:27:36 -0300

    Parte 1 Exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/scripts.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/scripts.js
@@ -1,16 +1,4 @@
-// function testingScope(escopo) { 
-//   if (escopo === true) { 
-//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-//     console.log(ifScope);
-//   } else {
-//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//     console.log(elseScope);
-//   }
-//   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-// }
-
-// testingScope(true);
+//PARTE 1 EXERCÍCIO 1
 
 const testingScope = escopo => {
   if (escopo === true) {
@@ -24,3 +12,11 @@ const testingScope = escopo => {
   //console.log(`${ifScope} o que estou fazendo aqui ? :O`);
 }
 testingScope(true);
+
+//PARTE 1 EXERCÍCIO 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a,b) => a - b);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
