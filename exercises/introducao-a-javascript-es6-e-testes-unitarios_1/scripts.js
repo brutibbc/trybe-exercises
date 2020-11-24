79292cf3324bf879bcb64b885c06096ccf90a927
brutibbc <brutibbc@gmail.com>
2020-11-24 08:42:39 -0300

    Aula 7.1 Parte 2 Exercicio 2

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/scripts.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/scripts.js
@@ -20,3 +20,28 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 oddsAndEvens.sort((a,b) => a - b);
 
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+//PARTE 2 EXERCÍCIO 1
+
+const fatorial = (num) => {
+  for (let index = (num-1); index > 1; index-=1) {
+    num = num * index;    
+  }
+  console.log(num);
+}
+fatorial(4);
+
+//PARTE 2 EXERCÍCIO 2
+const frase = [];
+let palavrao = '';
+const longestWord = (frase) => {
+  frase = frase.split(' ');
+  for (let index = 0; index < frase.length; index+=1) {
+    const element = frase[index];
+    if (element.length > palavrao.length) {
+      palavrao = element;
+    }
+  }
+  return palavrao;
+}
+console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")); // retorna 'aconteceu'
